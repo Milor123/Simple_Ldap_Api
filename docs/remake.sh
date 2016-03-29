731742284c9e4f66423fa8361fecf5ef4ff438f1
Milor123 <kora_bohorquez@hotmail.com>
2016-03-29 10:49:35 -0500

    docs

--- docs/remake.sh
+++ docs/remake.sh
@@ -3,6 +3,7 @@ if [ ! -d "Simple_Ldap_Api_gh-pages" ]; then
     git clone git@bitbucket.org:Milor123/simple_ldap_api.git 
     mv Simple_Ldap_Api Simple_Ldap_Api_gh-pages
     cd Simple_Ldap_Api_gh-pages
+    git branch gh-pages
     git checkout gh-pages
     cd ..
 fi
