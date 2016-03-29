92470e51ccd7543359fe57ea5ee9ab9528a4bb99
Milor123 <kora_bohorquez@hotmail.com>
2016-03-29 11:13:47 -0500

    no commit

--- docs/remake.sh
+++ docs/remake.sh
@@ -3,14 +3,13 @@ if [ ! -d "Simple_Ldap_Api_gh-pages" ]; then
     git clone git@bitbucket.org:Milor123/simple_ldap_api.git 
     mv simple_ldap_api simple_ldap_api_gh-pages
     cd simple_ldap_api_gh-pages
-    git branch gh-pages
     git checkout gh-pages
     cd ..
 fi
 cp -r _build/html/* simple_ldap_api_gh-pages/
 cd simple_ldap_api_gh-pages
-git push add .
+git add .
 git commit -m 'no commit'
 git push origin gh-pages
 cd ..
-#rm -r simple_ldap_api_gh-pages
+rm -r simple_ldap_api_gh-pages
